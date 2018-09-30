9978c5423b5aaa462a8b45ab83b7ad824dff5600
Léo Lam <leo@innovatetechnologi.es>
2018-09-30 10:43:33 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rstb",
-    version="1.0.3",
+    version="1.1.0",
     author="leoetlino",
     author_email="leo@leolam.fr",
     description="Breath of the Wild RSTB parser and editing tool",
